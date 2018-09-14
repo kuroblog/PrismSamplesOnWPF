c5ddb3fb3302aac2927537b7bcaa97faaf55dfcb
kuro <kuro_q@live.com>
2018-09-14 11:33:42 +0800

    ver2

---
+++ WpfApplication2/WpfApplication2.Config/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+namespace WpfApplication2.Config
+{
+    using System.Windows;
+
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
+    }
+}
