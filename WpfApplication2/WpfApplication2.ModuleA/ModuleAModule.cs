9a8e509def0924a4542a8a6fdf32a7e79c4c8516
kuro <kuro_q@live.com>
2018-09-14 11:18:54 +0800

    ver1

---
+++ WpfApplication2/WpfApplication2.ModuleA/ModuleAModule.cs
@@ -0,0 +1,22 @@
+﻿
+namespace WpfApplication2.ModuleA
+{
+    using Prism.Modularity;
+    using Prism.Regions;
+    using Views;
+
+    public class ModuleAModule : IModule
+    {
+        IRegionManager _regionManager;
+
+        public ModuleAModule(RegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+        }
+    }
+}
---
+++ WpfApplication2/WpfApplication2/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿
+namespace WpfApplication2
+{
+    using Microsoft.Practices.Unity;
+    using Prism.Modularity;
+    using Prism.Unity;
+    using System.Windows;
+
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+    }
+}
