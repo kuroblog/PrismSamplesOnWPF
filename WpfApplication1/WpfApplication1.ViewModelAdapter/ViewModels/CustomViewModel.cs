02a30331fe41cf1d02d5a5b9a937f7687b5775a2
kuro <kuro_q@live.com>
2018-09-14 15:46:48 +0800

    binging command

--- WpfApplication1/WpfApplication1.ViewModelAdapter/ViewModels/CustomViewModel.cs
+++ WpfApplication1/WpfApplication1.ViewModelAdapter/ViewModels/CustomViewModel.cs
@@ -1,7 +1,9 @@
 ﻿
 namespace WpfApplication1.ViewModelAdapter.ViewModels
 {
+    using Prism.Commands;
     using Prism.Mvvm;
+    using System;
 
     public class CustomViewModel : BindableBase
     {
@@ -10,6 +12,62 @@ namespace WpfApplication1.ViewModelAdapter.ViewModels
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
+        }
+
+        private bool _isEnabled;
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set
+            {
+                SetProperty(ref _isEnabled, value);
+                ExecuteDelegateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string _updateText;
+        public string UpdateText
+        {
+            get { return _updateText; }
+            set { SetProperty(ref _updateText, value); }
+        }
+
+        public DelegateCommand SampleCommand => new DelegateCommand(() => { });
+
+        public DelegateCommand SampleCommandEx => new DelegateCommand(() => { }, () => { return false; });
+
+        public DelegateCommand ExecuteDelegateCommand { get; private set; }
+
+        public DelegateCommand<string> ExecuteGenericDelegateCommand { get; private set; }
+
+        public DelegateCommand DelegateCommandObservesProperty { get; private set; }
+
+        public DelegateCommand DelegateCommandObservesCanExecute { get; private set; }
+
+        public CustomViewModel()
+        {
+            ExecuteDelegateCommand = new DelegateCommand(Execute, CanExecute);
+
+            DelegateCommandObservesProperty = new DelegateCommand(Execute, CanExecute).ObservesProperty(() => IsEnabled);
+
+            DelegateCommandObservesCanExecute = new DelegateCommand(Execute).ObservesCanExecute(() => IsEnabled);
+
+            ExecuteGenericDelegateCommand = new DelegateCommand<string>(ExecuteGeneric).ObservesCanExecute(() => IsEnabled);
+        }
+
+        private void Execute()
+        {
+            UpdateText = $"Updated: {DateTime.Now}";
+        }
+
+        private void ExecuteGeneric(string parameter)
+        {
+            UpdateText = parameter;
+        }
+
+        private bool CanExecute()
+        {
+            return IsEnabled;
         }
     }
 }
