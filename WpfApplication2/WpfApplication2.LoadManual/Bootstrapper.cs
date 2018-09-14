121df5bf02f92011614aa803553fd0d51f6c875b
kuro <kuro_q@live.com>
2018-09-14 11:55:49 +0800

    ver5

---
+++ WpfApplication2/WpfApplication2.LoadManual/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿
+namespace WpfApplication2.LoadManual
+{
+    using Microsoft.Practices.Unity;
+    using ModuleA;
+    using Prism.Modularity;
+    using Prism.Unity;
+    using System.Windows;
+
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleAType = typeof(ModuleAModule);
+            ModuleCatalog.AddModule(new ModuleInfo()
+            {
+                ModuleName = moduleAType.Name,
+                ModuleType = moduleAType.AssemblyQualifiedName,
+                InitializationMode = InitializationMode.OnDemand
+            });
+        }
+    }
+}
---
+++ WpfApplication2/WpfApplication2.LoadManual/ShellWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿
+namespace WpfApplication2.LoadManual
+{
+    using Prism.Modularity;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+
+    public partial class ShellWindow : Window
+    {
+        IModuleManager _moduleManager;
+
+        public ShellWindow(IModuleManager moduleManager)
+        {
+            InitializeComponent();
+            _moduleManager = moduleManager;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _moduleManager.LoadModule("ModuleAModule");
+        }
+    }
+}
