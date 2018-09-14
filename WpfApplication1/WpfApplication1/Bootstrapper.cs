adc042696843992a6f1680adbb883fb26949edde
kuro <kuro_q@live.com>
2018-09-14 14:03:56 +0800

    autowire viewmode/change autowire rule

--- WpfApplication1/WpfApplication1/Bootstrapper.cs
+++ WpfApplication1/WpfApplication1/Bootstrapper.cs
@@ -2,7 +2,10 @@
 namespace WpfApplication1
 {
     using Microsoft.Practices.Unity;
+    using Prism.Mvvm;
     using Prism.Unity;
+    using System;
+    using System.Reflection;
     using System.Windows;
 
     class Bootstrapper : UnityBootstrapper
@@ -15,6 +18,27 @@ namespace WpfApplication1
         protected override void InitializeShell()
         {
             Application.Current.MainWindow.Show();
+        }
+
+        /// <summary>
+        /// 使用 prism:ViewModelLocator.AutoWireViewModel="True" 的方法，必须遵循以下规则
+        /// ViewModel 的名称必须是 View + ViewModel
+        /// 比如：
+        /// View：       ExampleView
+        /// ViewModel：  ExampleViewViewModel
+        /// 这里重写基类的该方法以达到改变自动匹配的规则
+        /// </summary>
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewName = viewType.FullName;
+                var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+                var viewModelName = $"{viewName}ViewModel, {viewAssemblyName}";
+                return Type.GetType(viewModelName);
+            });
         }
     }
 }
