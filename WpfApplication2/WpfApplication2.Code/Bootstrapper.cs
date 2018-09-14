43482e3eff46124a5c5ebd24a0447a4d9497ac84
kuro <kuro_q@live.com>
2018-09-14 11:39:52 +0800

    ver3

---
+++ WpfApplication2/WpfApplication2.Code/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿
+namespace WpfApplication2.Code
+{
+    using Microsoft.Practices.Unity;
+    using ModuleA;
+    using Prism.Modularity;
+    using Prism.Unity;
+    using System.Windows;
+
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var catalog = (ModuleCatalog)ModuleCatalog;
+            catalog.AddModule(typeof(ModuleAModule));
+        }
+    }
+}
