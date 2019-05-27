ad6ad2395773e9543b20fd1a700a611707ada511
Yuqi.Zhao <temp777@yeah.net>
2019-05-27 16:18:55 +0800

    add converter

---
+++ PrismExtensibilityFramework/PEF.Common/Converters/ErrorContentConverter.cs
@@ -0,0 +1,20 @@
+﻿
+namespace PEF.Common.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class ErrorContentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
