cbf619f89d5a4763dfe3d7a73b1afa970e5c767a
Yuqi.Zhao <temp777@yeah.net>
2019-05-27 09:50:45 +0800

    set styles

---
+++ PrismExtensibilityFramework/PEF.Common/Converters/FontSizeConverter.cs
@@ -0,0 +1,23 @@
+﻿
+namespace PEF.Common.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class FontSizeConverter : IValueConverter
+    {
+        private const double default_font_size = 14.666;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try { return value == null ? default_font_size : value is double ? System.Convert.ToDouble(value) * 96 / 72 : default_font_size; }
+            catch { return default_font_size; }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
