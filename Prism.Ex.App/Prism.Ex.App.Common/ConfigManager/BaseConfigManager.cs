4dd1017630edd2c715af83729a3af8be7dcde40f
Yuqi.Zhao <temp777@yeah.net>
2019-09-16 09:43:26 +0800

    添加配置读取类

---
+++ Prism.Ex.App/Prism.Ex.App.Common/ConfigManager/BaseConfigManager.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Prism.Ex.App.Common
+{
+    using System.Configuration;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    public abstract class BaseConfigManager : IConfigManager
+    {
+        public virtual string ReadAppSetting(string key) => ConfigurationManager.OpenMappedExeConfiguration(new ExeConfigurationFileMap
+        {
+            ExeConfigFilename = string.Concat(Assembly.GetCallingAssembly().Location, ".config")
+        }, ConfigurationUserLevel.None).AppSettings.Settings[key].Value;
+
+        public virtual void SaveAppSetting(string key, string value)
+        {
+            var config = ConfigurationManager.OpenMappedExeConfiguration(new ExeConfigurationFileMap
+            {
+                ExeConfigFilename = string.Concat(Assembly.GetCallingAssembly().Location, ".config")
+            }, ConfigurationUserLevel.None);
+
+            if (config.AppSettings.Settings.AllKeys.Contains(key))
+            {
+                var result = config.AppSettings.Settings[key].Value;
+                if (result != value)
+                {
+                    config.AppSettings.Settings[key].Value = value;
+                    config.Save(ConfigurationSaveMode.Modified);
+                }
+            }
+            else
+            {
+                config.AppSettings.Settings.Add(key, value);
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+        }
+
+        public virtual string ReadConnectionString(string key) => ConfigurationManager.OpenMappedExeConfiguration(new ExeConfigurationFileMap
+        {
+            ExeConfigFilename = string.Concat(Assembly.GetCallingAssembly().Location, ".config")
+        }, ConfigurationUserLevel.None).ConnectionStrings.ConnectionStrings[key].ConnectionString;
+
+        public virtual string ReadAllText(string path) => File.ReadAllText(path);
+    }
+}
---
+++ Prism.Ex.App/Prism.Ex.App.Common/ConfigManager/IConfigManager.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Prism.Ex.App.Common
+{
+    public interface IConfigManager
+    {
+        string ReadAppSetting(string key);
+
+        void SaveAppSetting(string key, string value);
+
+        string ReadConnectionString(string key);
+
+        string ReadAllText(string path);
+    }
+}
